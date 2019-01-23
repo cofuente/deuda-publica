53bcca7dc1be654db300d7fb077762b0c7ad782a
Chiara Marcial Martínez <cmarmar@gmail.com>
2019-01-23 01:24:27 -0500

    Updates

---
+++ precache-manifest.3e4f2abbf2b5d783b09befdcfa88485c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d3452ef269fc2193547a",
+    "url": "/deuda-publica/static/js/runtime~main.d3452ef2.js"
+  },
+  {
+    "revision": "d268b4b2873c2978eb94",
+    "url": "/deuda-publica/static/js/main.d268b4b2.chunk.js"
+  },
+  {
+    "revision": "ef668dd0ce8318d52827",
+    "url": "/deuda-publica/static/js/1.ef668dd0.chunk.js"
+  },
+  {
+    "revision": "d268b4b2873c2978eb94",
+    "url": "/deuda-publica/static/css/main.c77c6142.chunk.css"
+  },
+  {
+    "revision": "94e404fa19ff1dff17c15468d6373b35",
+    "url": "/deuda-publica/index.html"
+  }
+];
