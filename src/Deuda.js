b9177d770036d74347e959b6ff5bf773b2fa53af
Chiara Marcial Martínez <cmarmar@gmail.com>
2019-01-23 19:33:02 -0500

    adjusted interes rates and entrance animations

--- src/Deuda.js
+++ src/Deuda.js
@@ -5,37 +5,45 @@ export default class Deuda extends Component {
     constructor(props){
         super(props)
         this.state = {
-            deuda: 70000000000,
-            deudaInicial: 70000000000,
-            initialLoadTime: new Date(),
-            interestRatePerSecond: .00033333333
-            //Anual interest rate /
+            totalInterest: 0,
+            interestIncrements: 0,
+            pageLoad: new Date(),
+            interestRatePerMS: 0.155377980720446479124,  // this should be the anual interest rate (here I used 7) divided by 31536000000, the number of milliseconds in a year
+            dateBaseline: 1487160000000, // mid january 2017 announcement
+            debtBaseline: 7e+10, // 70,000,000,000
         }
         this.componentDidMount = this.componentDidMount.bind(this)
         this.componentWillUnmount = this.componentWillUnmount.bind(this)
         this.tick = this.tick.bind(this)
     }
+
     componentDidMount(){
+      const timeSinceBaseline = this.state.pageLoad-this.state.dateBaseline
+      const totalInterestThusFar = this.state.interestRatePerMS * timeSinceBaseline
+      const interestIncrements = this.state.interestRatePerMS * 100
+      this.setState({totalInterest: totalInterestThusFar, interestIncrements: interestIncrements })
       this.timerID = setInterval(
         () => this.tick(),
         100
       )
     }
+
     componentWillUnmount(){
       clearInterval(this.timerID)
     }
+
     tick(){
       this.setState((state, props) => ({
-        deuda: state.deudaInicial + ((new Date()- state.initialLoadTime) * state.interestRatePerSecond),
+        totalInterest: state.totalInterest + state.interestIncrements,
       }))
     }
 
   render() {
-      const deuda = this.state.deuda.toFixed(2)
+      const deuda = Number(this.state.totalInterest) + this.state.debtBaseline
       return (
         <div className="deuda-container">
           <p className="text-pop-up-top">
-            <CurrencyFormat className="deuda" value={deuda} displayType={'text'} thousandSeparator={true} prefix={'$'}  />
+            <CurrencyFormat className="deuda" value={deuda.toFixed(2)} displayType={'text'} thousandSeparator={true} prefix={'$'}  />
           </p>
         </div>
     )
